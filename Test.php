f48b7fde5c97b7aa5bc9cfa7df4158429afa5cea
Kevin Rudissaar <krudissaar@nhbs.local>
2017-02-21 08:54:52 +0000

    Changed session to cookies.

--- Test.php
+++ Test.php
@@ -7,17 +7,18 @@ use yii\base\Component;
 use yii\base\Exception;
 use yii\filters\AccessControl;
 use yii\helpers\Inflector;
+use yii\web\Cookie;
 
 class Test extends Component
 {
     public $filter;
     public $tests;
-    
+
     public function prefix()
     {
         return 'yii2_split_test_';
     }
-    
+
     public function key($v)
     {
         return $this->prefix() . Inflector::slug($v, '_');
@@ -28,15 +29,14 @@ class Test extends Component
         if(!$test = $this->getTest($name)){
             throw new Exception('Test "' . $name . '" is not in configuration');
         }
-        
-        $session = Yii::$app->getSession();
+
         $key = $this->key($name);
-        
+
         if(!isset($test['values']) || !is_array($test['values'])){
             throw new Exception('Test "' . $name . '" requires an array of values');
         }
         $values = $test['values'];
-        
+
         $filter = isset($test['filter']) ? $test['filter'] : $this->filter;
         if($filter){
             if(!isset($test['default'])){
@@ -44,72 +44,83 @@ class Test extends Component
                     'Test "' . $name . '" requires a "default" parameter and value'
                 );
             }
-        
+
             // Add a default rule for allowing it all
             $filter['rules'][] = ['allow' => true];
-            
+
             $o = Yii::createObject(
                 array_merge(
                     [
-                        'class' => AccessControl::class, 
-                    ], 
+                        'class' => AccessControl::class,
+                    ],
                     $filter
                 )
             );
             if(!$this->can($o)){
-                $session->remove($key);
+                $cookies = Yii::$app->response->cookies;
+                $cookies->remove($key);
                 return $test['default'];
             }
         }
-        
-        $active = $session->get($key);
-        
+
+        $cookies = Yii::$app->request->cookies;
+        $active = false;
+        if($cookies->has($key)) {
+            $active = explode(';', $cookies->getValue($key))[1];
+        }
+
         if(!$active || !in_array($active, $values)){
             // Pick a test randomly
             $active = $values[array_rand($values)];
-            $session->set($key, [$name, $active]);
+            $cookies = Yii::$app->response->cookies;
+            $cookie = new Cookie([
+                'name' => $key,
+                'value' => $name . ';' . $active,
+                'expire' => 0
+            ]);
+            Yii::$app->getResponse()->getCookies()->add($cookie);
         }
-        
+
         return $active;
     }
-    
+
     public function listTests($allActions = false)
     {
-        $session = Yii::$app->getSession();
+        $cookies = Yii::$app->request->cookies;
         $list = [];
         foreach($this->tests as $k => $v){
             if(!isset($v['name'])){
                 continue;
             }
             $name = $v['name'];
-            
-            $sessionTest = $session->get($this->key($name));
-            if($sessionTest && is_array($sessionTest)){
-                $test = $this->getTest($sessionTest[0]);
+
+            $cookieTest = explode(';', $cookies[$this->key($name)]);
+            if($cookieTest && is_array($cookieTest)){
+                $test = $this->getTest($cookieTest[0]);
                 unset($test['values']);
-                
+
                 if(!$allActions && isset($test['action'])){
-                    $currentAction = Yii::$app->controller->id . '/' 
-                        . Yii::$app->controller->action->id;    
-                    
+                    $currentAction = Yii::$app->controller->id . '/'
+                        . Yii::$app->controller->action->id;
+
                     if(
-                        is_array($test['action']) && 
+                        is_array($test['action']) &&
                         !in_array($currentAction, $test['action'])
                     ){
                         continue;
                     }elseif(
-                        !is_array($test['action']) && 
+                        !is_array($test['action']) &&
                         $currentAction !== $test['action']
                     ){
                         continue;
                     }
                 }
-                
+
                 if($test){
                     $list[$name] = array_merge(
-                        $test, 
+                        $test,
                         [
-                            'value' => $sessionTest[1]
+                            'value' => $cookieTest[1]
                         ]
                     );
                 }
@@ -117,7 +128,7 @@ class Test extends Component
         }
         return $list;
     }
-    
+
     public function getTest($name)
     {
         foreach($this->tests as $k => $v){
@@ -126,7 +137,7 @@ class Test extends Component
             }
         }
     }
-    
+
     public function can($ac)
     {
         $user = $ac->user;
